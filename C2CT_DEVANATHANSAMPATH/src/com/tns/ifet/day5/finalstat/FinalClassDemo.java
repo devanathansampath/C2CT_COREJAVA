b0819a6e21ee3979fe0b56976d2e26a5fb5c3caa
Dell <Dell@devanathansampath>
2024-11-06 12:29:53 +0530

    day5 program

---
+++ C2CT_DEVANATHANSAMPATH/src/com/tns/ifet/day5/finalstat/FinalClassDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.day5.finalstat;
+
+public class FinalClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
+		f1.show();
+
+	}
+
+}
---
+++ C2CT_DEVANATHANSAMPATH/src/com/tns/ifet/day5/finalstat/FinalMethodClassTest.java
@@ -0,0 +1,5 @@
+package com.tns.ifet.day5.finalstat;
+
+public class FinalMethodClassTest extends FinalMethodClass {
+
+}
