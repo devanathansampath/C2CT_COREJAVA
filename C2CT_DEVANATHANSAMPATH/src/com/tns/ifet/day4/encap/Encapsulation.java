33dfb6ad7543d5ae6267a63e7d442497fd725d8e
Dell <Dell@devanathansampath>
2024-11-06 12:11:16 +0530

    day4 program

---
+++ C2CT_DEVANATHANSAMPATH/src/com/tns/ifet/day4/encap/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.day4.encap;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConcept obj = new 	OopsConcept(); 
+				obj.setSerialNum(101); 
+				obj.setName("Pooja"); 
+				obj.setAge(20); 
+				System.out.println(obj); 
+
+	}
+
+}
---
+++ C2CT_DEVANATHANSAMPATH/src/com/tns/ifet/day4/sing/inher/SLInheritanceDemo.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.day4.sing.inher;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City 	College"); 
+				System.out.println(student);
+		
+
+	}
+
+}
