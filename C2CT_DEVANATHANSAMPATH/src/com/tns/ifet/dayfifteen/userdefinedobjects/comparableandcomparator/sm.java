1484e8b9fd3c70ed17e3b014b48fcd46be14f194
Dell <Dell@devanathansampath>
2024-11-14 09:54:18 +0530

    day 15 and 16

---
+++ C2CT_DEVANATHANSAMPATH/src/com/tns/ifet/dayfifteen/userdefinedobjects/comparableandcomparator/sm.java
@@ -0,0 +1,5 @@
+package com.tns.ifet.dayfifteen.userdefinedobjects.comparableandcomparator;
+
+public class sm {
+
+}
---
+++ C2CT_DEVANATHANSAMPATH/src/com/tns/ifet/daysixteen/queue/fs.java
@@ -0,0 +1,5 @@
+package com.tns.ifet.daysixteen.queue;
+
+public class fs {
+
+}
